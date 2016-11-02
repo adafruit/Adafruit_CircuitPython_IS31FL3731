4e0000bde0e55db93653b583322de722c9fc2d74
Tony DiCola <tony@tonydicola.com>
2016-11-02 11:24:38 -0700

    Minor version bump to get new release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='micropython-adafruit-is31fl3731',
     py_modules=['is31fl3731'],
-    version="1.0",
+    version="1.0.1",
     description="Driver for MicroPython for the IS31FL3731 LED matrix.",
     long_description="""\
 Driver for the IS31FL3731-based charlieplexed LED matrices and CharlieWing.""",
