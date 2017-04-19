bfa8c9c91ad43f51823abb0d5742c015147a818a
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-04-19 14:28:34 -0700

    Fix up the actual code and switch to an API modeled after PIL/pillow PixelAccess.

--- README.rst
+++ README.rst
@@ -18,6 +18,7 @@ This driver depends on:
 
 * `Adafruit CircuitPython <https://github.com/adafruit/circuitpython>`_
 * `Bus Device <https://github.com/adafruit/Adafruit_CircuitPython_BusDevice>`_
+* `Register <https://github.com/adafruit/Adafruit_CircuitPython_Register>`_
 
 Please ensure all dependencies are available on the CircuitPython filesystem.
 This is easily achieved by downloading
@@ -28,20 +29,28 @@ Usage Example
 
 Matrix::
 
-    import is31fl3731
-    from machine import I2C, Pin
-    i2c = I2C(Pin(5), Pin(4))
-    display = is31fl3731.Matrix(i2c)
-    display.fill(127)
+    import adafruit_is31fl3731
+    import board
+    import busio
+    with busio.I2C(board.SCL, board.SDA) as i2c:
+        display = adafruit_is31fl3731.Matrix(i2c)
+        display.fill(127)
 
 
 Charlie Wing::
 
-    import is31fl3731
-    from machine import I2C, Pin
-    i2c = I2C(Pin(5), Pin(4))
-    display = is31fl3731.CharlieWing(i2c)
-    display.fill(127)
+    import adafruit_is31fl3731
+    import board
+    import busio
+    with busio.I2C(board.SCL, board.SDA) as i2c:
+        display = is31fl3731.CharlieWing(i2c)
+        display.fill(127)
+
+        # Turn off pixel 4,4, change its brightness and turn it back on
+        frame = display.displayed_frame
+        frame.on[4,4] = False
+        frame.brightness[4,4] = 192
+        frame.on[4,4] = True
 
 Contributing
 ============
--- adafruit_is31fl3731.py
+++ adafruit_is31fl3731.py
@@ -1,6 +1,7 @@
 # The MIT License (MIT)
 #
 # Copyright (c) 2016 Radomir Dopieralski
+# Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,15 +24,20 @@
 `adafruit_is31fl3731`
 ====================================================
 
-TODO(description)
+Basic driver for the IS31FL3731 a charlieplexed LED driver.
 
-* Author(s): Radomir Dopieralski
+* Author(s): Radomir Dopieralski, Scott Shawcroft
+* Datasheet: http://www.issi.com/WW/pdf/31FL3731.pdf
 """
 
 import math
 import time
 
-_MODE_REGISTER = const(0x00)
+from adafruit_bus_device.i2c_device import I2CDevice
+from adafruit_register import i2c_bit
+from adafruit_register import i2c_bits
+from adafruit_register import i2c_struct
+
 _FRAME_REGISTER = const(0x01)
 _AUTOPLAY1_REGISTER = const(0x02)
 _AUTOPLAY2_REGISTER = const(0x03)
@@ -50,197 +56,167 @@ _PICTURE_MODE = const(0x00)
 _AUTOPLAY_MODE = const(0x08)
 _AUDIOPLAY_MODE = const(0x18)
 
-_ENABLE_OFFSET = const(0x00)
-_BLINK_OFFSET = const(0x12)
-_COLOR_OFFSET = const(0x24)
+class ByteArray:
+    """Provides byte access via a 2D array using index like [1,2] where 1 is x and 2 is y."""
+    def __init__(self, parent, frame, register):
+        self.parent = parent
+        self.register = register
+        self.buf = bytearray(2)
+        self.frame = frame
+
+    def __getitem__(self, index):
+        addr = self.parent._pixel_addr(*index)
+        self.parent.edit_frame = self.frame
+
+        with self.parent.i2c_device as i2c:
+            self.buf[0] = self.register + addr
+            i2c.write(self.buf, end=1)
+            i2c.read_into(self.buf[0], start=1)
+
+        return self.buf[1]
+
+    def __setitem__(self, index, val):
+        addr = self.parent._pixel_addr(*index)
+        self.parent.edit_frame = self.frame
+
+        with self.parent.i2c_device as i2c:
+            self.buf[0] = self.register + addr
+            self.buf[1] = val
+            i2c.write(self.buf)
+
+class BitArray:
+    """Provides bit access via a 2D array using index like [1,2] where 1 is x and 2 is y."""
+    def __init__(self, parent, frame, register):
+        self.parent = parent
+        self.register = register
+        self.buf = bytearray(2)
+        self.frame = frame
+
+    def _read_bits(self, index):
+        addr = self.parent._pixel_addr(*index)
+        addr //= 8
+        self.parent.edit_frame = self.frame
+
+        with self.parent.i2c_device as i2c:
+            self.buf[0] = self.register + addr
+            i2c.write(self.buf, end=1)
+            i2c.read_into(self.buf, start=1)
+        return self.buf[1]
+
+    def __getitem__(self, index):
+        return (self._read_bits(index) & (1 << bit)) != 0
+
+    def __setitem__(self, index, val):
+        addr = self.parent._pixel_addr(*index)
+        bit = addr % 8
+        addr //= 8
+
+        bits = self._read_bits(index)
+        if val:
+            bits |= 1 << bit
+        else:
+            bits &= ~(1 << bit)
+
+        with self.parent.i2c_device as i2c:
+            self.buf[0] = self.register + addr
+            self.buf[1] = bits
+            i2c.write(self.buf)
+
+import time
+
+class Frame:
+    def __init__(self, parent):
+        self.parent = parent
+        self.brightness = ByteArray(parent, self, 0x24)
+        """Pixel array model after pixel arrays in PIL"""
+        self.on = BitArray(parent, self, 0x0)
+        self.blink = BitArray(parent, self, 0x12)
+
+    def fill(self, color, blink=False):
+        self.parent.edit_frame = self
+        # TODO(tannewt): Bulk fill brightness and blink for speed.
+        # TODO(tannewt): Turn on each pixel once at the start.
+        for y in range(4):
+            for x in range(4):
+                self.on[x,y] = True
+                self.brightness[x,y] = color
+                self.blink[x,y] = blink
 
 class Matrix:
     """Charlieplexed 16x9 LED matrix."""
     width = 16
     height = 9
 
+    # Known as the shutdown register but 1 is on and 0 is shutdown.
+    _on = i2c_bit.RWBit(_SHUTDOWN_REGISTER, 0)
+    """Whether the display is on."""
+
+    _bank = i2c_struct.UnaryStruct(_BANK_ADDRESS, "B")
+    _blink_period_time = i2c_bits.RWBits(3, 0x5, 0)
+    _blink_enable = i2c_bit.RWBit(0x5, 0)
+    _mode = i2c_bits.RWBits(2, 0x0, 0x3)
+
+    # TODO(tannewt): Support autoplay and breath animations. Perhaps use a
+    # separate module with subclasses to reduce memory footprint.
+
     def __init__(self, i2c, address=0x74):
-        self.i2c = i2c
-        self.address = address
-        self.reset()
-        self.init()
+        self.i2c_device = I2CDevice(i2c, address)
+        # Cache the active frame/bank to reduce I2C traffic.
+        self._bank_cache = 9
+        self.edit_frame = None
 
-    def _bank(self, bank=None):
-        if bank is None:
-            return self.i2c.readfrom_mem(self.address, _BANK_ADDRESS, 1)[0]
-        self.i2c.writeto_mem(self.address, _BANK_ADDRESS, bytearray([bank]))
+        # Turn off LED outputs
+        self._on = False
 
-    def _register(self, bank, register, value=None):
-        self._bank(bank)
-        if value is None:
-            return self.i2c.readfrom_mem(self.address, register, 1)[0]
-        self.i2c.writeto_mem(self.address, register, bytearray([value]))
+        # Setup the registers
+        self._mode = _PICTURE_MODE
+        self.frames = []
+        for i in range(8):
+            frame = Frame(self)
+            self.frames.append(frame)
 
-    def _mode(self, mode=None):
-        return self._register(_CONFIG_BANK, _MODE_REGISTER, mode)
+            # Blank out the frame memory.
+            self.edit_frame = frame
+            zeroes = bytearray(0xb3 + 1)
+            with self.i2c_device as i2c:
+                i2c.write(zeroes)
+        self.displayed_frame = self.frames[0]
 
-    def init(self):
-        self._mode(_PICTURE_MODE)
-        self.frame(0)
-        for frame in range(8):
-            self.fill(0, False, frame=frame)
-            for col in range(18):
-                self._register(frame, _ENABLE_OFFSET + col, 0xff)
-        self.audio_sync(False)
+        # Turn on the display
+        self.edit_frame = None
+        self._on = True
 
-    def reset(self):
-        """Reset the matrix."""
-        self.sleep(True)
-        utime.sleep_us(10)
-        self.sleep(False)
+    @property
+    def edit_frame(self):
+        """Current frame being edited. None if config."""
+        if self._bank_cache == _CONFIG_BANK:
+            return None
+        return self.frames[self._bank_cache]
 
-    def sleep(self, value):
-        """Get or set the sleep mode."""
-        return self._register(_CONFIG_BANK, _SHUTDOWN_REGISTER, not value)
+    @edit_frame.setter
+    def edit_frame(self, frame):
+        bank = _CONFIG_BANK
+        if frame != None:
+            bank = self.frames.index(frame)
 
-    def autoplay(self, delay=0, loops=0, frames=0):
-        """Enables or disables autoplay.
+        if self._bank_cache != bank:
+            self._bank_cache = bank
+            self._bank = bank
 
-        If ``delay`` is 0, autoplay is disabled. Otherwise the display will
-        switch between ``frames`` frames every ``delay`` milliseconds, and
-        repeat the cycle ``loops`` times.  If ``loops`` is 0, it will repeat
-        indefinitely."""
-        if delay == 0:
-            self._mode(_PICTURE_MODE)
-            return
-        delay //= 11
-        if not 0 <= loops <= 7:
-            raise ValueError("Loops out of range")
-        if not 0 <= frames <= 7:
-            raise ValueError("Frames out of range")
-        if not 1 <= delay <= 64:
-            raise ValueError("Delay out of range")
-        self._register(_CONFIG_BANK, _AUTOPLAY1_REGISTER, loops << 4 | frames)
-        self._register(_CONFIG_BANK, _AUTOPLAY2_REGISTER, delay % 64)
-        self._mode(_AUTOPLAY_MODE | self._frame)
+    @property
+    def blink_period(self):
+        """Blink period up to 1890ms in steps of 270ms."""
+        self.edit_frame = None
+        return self._blink_period * 270
 
-    def fade(self, fade_in=None, fade_out=None, pause=0):
-        """Disables or enables and configures fading.
-
-        If called without parameters, disables fading. If ``fade_in`` and/or
-        ``fade_out`` are specified, it will take that many milliseconds to
-        change between frames, with ``pause`` milliseconds of dark between."""
-        if fade_in is None and fade_out is None:
-            self._register(_CONFIG_BANK, _BREATH2_REGISTER, 0)
-        elif fade_in is None:
-            fade_in = fade_out
-        elif fade_out is None:
-            fade_out = fade_in
-        fade_in = int(math.log(fade_in / 26, 2))
-        fade_out = int(math.log(fade_out / 26, 2))
-        pause = int(math.log(pause / 26, 2))
-        if not 0 <= fade_in <= 7:
-            raise ValueError("Fade in out of range")
-        if not 0 <= fade_out <= 7:
-            raise ValueError("Fade out out of range")
-        if not 0 <= pause <= 7:
-            raise ValueError("Pause out of range")
-        self._register(_CONFIG_BANK, _BREATH1_REGISTER, fade_out << 4 | fade_in)
-        self._register(_CONFIG_BANK, _BREATH2_REGISTER, 1 << 4 | pause)
-
-    def frame(self, frame=None, show=True):
-        """Change or get active frame.
-
-        If ``frame`` is not specified, returns the active frame, otherwise sets
-        it to the value of ``frame``. If ``show`` is ``True``, also shows that
-        frame."""
-        if frame is None:
-            return self._frame
-        if not 0 <= frame <= 8:
-            raise ValueError("Frame out of range")
-        self._frame = frame
-        if show:
-            self._register(_CONFIG_BANK, _FRAME_REGISTER, frame);
-
-    def audio_sync(self, value=None):
-        """Enable, disable or get sync of brightness with audio input."""
-        return self._register(_CONFIG_BANK, _AUDIOSYNC_REGISTER, value)
-
-    def audio_play(self, sample_rate, audio_gain=0,
-                   agc_enable=False, agc_fast=False):
-        """Enable or disable frame display according to the audio input.
-
-        The ``sample_rate`` specifies sample rate in microseconds. If it is 0,
-        disable the audio play. The ``audio_gain`` specifies amplification
-        between 0dB and 21dB."""
-        if sample_rate == 0:
-            self._mode(_PICTURE_MODE)
-            return
-        sample_rate //= 46
-        if not 1 <= sample_rate <= 256:
-            raise ValueError("Sample rate out of range")
-        self._register(_CONFIG_BANK, _ADC_REGISTER, sample_rate % 256)
-        audio_gain //= 3
-        if not 0 <= audio_gain <= 7:
-            raise ValueError("Audio gain out of range")
-        self._register(_CONFIG_BANK, _GAIN_REGISTER,
-                       bool(agc_enable) << 3 | bool(agc_fast) << 4 | audio_gain)
-        self._mode(_AUDIOPLAY_MODE)
-
-    def blink(self, rate=None):
-        """Get or set blink rate up to 1890ms in steps of 270ms."""
-        if rate is None:
-            return (self._register(_CONFIG_BANK, _BLINK_REGISTER) & 0x07) * 270
-        elif rate == 0:
-            self._register(_CONFIG_BANK, _BLINK_REGISTER, 0x00)
-            return
-        rate //= 270
-        self._register(_CONFIG_BANK, _BLINK_REGISTER, rate & 0x07 | 0x08)
-
-    def fill(self, color=None, blink=None, frame=None):
-        """Fill the display with specified color and/or blink."""
-        if frame is None:
-            frame = self._frame
-        self._bank(frame)
-        if color is not None:
-            if not 0 <= color <= 255:
-                raise ValueError("Color out of range")
-            data = bytearray([color] * 24)
-            for row in range(6):
-                self.i2c.writeto_mem(self.address,
-                                     _COLOR_OFFSET + row * 24, data)
-        if blink is not None:
-            data = bool(blink) * 0xff
-            for col in range(18):
-                self._register(frame, _BLINK_OFFSET + col, data)
+    @blink_period.setter
+    def blink_period(self, period):
+        period //= 270
+        self.edit_frame = None
+        self._blink_period = period
 
     def _pixel_addr(self, x, y):
         return x + y * 16
-
-    def pixel(self, x, y, color=None, blink=None, frame=None):
-        """Read or write the specified pixel.
-
-        If ``color`` is not specified, returns the current value of the pixel,
-        otherwise sets it to the value of ``color``. If ``frame`` is not
-        specified, affects the currently active frame. If ``blink`` is
-        specified, it enables or disables blinking for that pixel."""
-        if not 0 <= x <= self.width:
-            return
-        if not 0 <= y <= self.height:
-            return
-        pixel = self._pixel_addr(x, y)
-        if color is None and blink is None:
-            return self._register(self._frame, pixel)
-        if frame is None:
-            frame = self._frame
-        if color is not None:
-            if not 0 <= color <= 255:
-                raise ValueError("Color out of range")
-            self._register(frame, _COLOR_OFFSET + pixel, color)
-        if blink is not None:
-            addr, bit = divmod(pixel, 8)
-            bits = self._register(frame, _BLINK_OFFSET + addr)
-            if blink:
-                bits |= 1 << bit
-            else:
-                bits &= ~(1 << bit)
-            self._register(frame, _BLINK_OFFSET + addr, bits)
-
 
 class CharlieWing(Matrix):
     """Driver for the 15x7 CharlieWing Adafruit FeatherWing."""
