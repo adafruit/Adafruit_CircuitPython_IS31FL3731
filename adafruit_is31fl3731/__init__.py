bdc98a4890dc3f5b835cfbbcbbe1d83d37521af3
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_is31fl3731/__init__.py
+++ adafruit_is31fl3731/__init__.py
@@ -55,7 +55,7 @@ from micropython import const
 
 from adafruit_bus_device.i2c_device import I2CDevice
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_IS31FL3731.git"
 
 _MODE_REGISTER = const(0x00)
