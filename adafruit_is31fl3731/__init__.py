4dc90541ff8bf32f748fcf11a6762f2c33360e5f
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2024-04-02 08:57:26 -0400

    delete and skip this part?

--- adafruit_is31fl3731/__init__.py
+++ adafruit_is31fl3731/__init__.py
@@ -118,7 +118,7 @@ class IS31FL3731:
 
     def _i2c_read_reg(
         self, reg: Optional[int] = None, result: Optional[ReadableBuffer] = None
-    ) -> None:
+    ):
         # Read a buffer of data from the specified 8-bit I2C register address.
         # The provided result parameter will be filled to capacity with bytes
         # of data read from the register.
