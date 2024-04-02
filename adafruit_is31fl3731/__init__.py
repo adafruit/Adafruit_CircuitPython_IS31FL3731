4fb28c3a1e2f8dba9b02ceb657cb096aace82ad0
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2024-04-02 09:24:11 -0400

    can has WriteableBuffer?

--- adafruit_is31fl3731/__init__.py
+++ adafruit_is31fl3731/__init__.py
@@ -116,7 +116,7 @@ class IS31FL3731:
         self._frame = None
         self._init(frames=frames)
 
-    def _i2c_read_reg(self, reg: Optional[int] = None, result: Optional[int] = None):
+    def _i2c_read_reg(self, reg: Optional[int] = None, result: Optional[WritableBuffer] = None):
         # Read a buffer of data from the specified 8-bit I2C register address.
         # The provided result parameter will be filled to capacity with bytes
         # of data read from the register.
