c80cc25ba9d3a73d5370d39c9755f3072b92abd8
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2024-04-02 11:22:06 -0400

    Leroyyyyy Jenkins

--- adafruit_is31fl3731/__init__.py
+++ adafruit_is31fl3731/__init__.py
@@ -59,9 +59,7 @@ import busio
 try:
     from typing import TYPE_CHECKING, List, Tuple, Optional, Iterable
     from PIL import Image
-
-    if TYPE_CHECKING:
-        from circuitpython_typing import ReadableBuffer, WriteableBuffer
+    from circuitpython_typing import ReadableBuffer, WriteableBuffer
 except ImportError as e:
     pass
 
